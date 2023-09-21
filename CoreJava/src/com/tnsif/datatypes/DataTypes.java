f99510ada5b99faacfeb51a78a672b01ca2cd5a9
ozairkhan2810 <ozairkhan2810@gmail.com>
2023-09-21 12:56:15 +0530

    DataTypes

---
+++ CoreJava/src/com/tnsif/datatypes/DataTypes.java
@@ -0,0 +1,13 @@
+package com.tnsif.datatypes;
+
+public class DataTypes {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+		int a = 2;
+		System.out.println(a);
+		
+	}
+
+}
