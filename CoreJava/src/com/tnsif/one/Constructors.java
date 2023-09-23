cb3b1e7e2994af0b805aa23f403cf35a6bf3b420
ozairkhan2810 <ozairkhan2810@gmail.com>
2023-09-23 17:53:06 +0530

    my comit

---
+++ CoreJava/src/com/tnsif/one/Constructors.java
@@ -0,0 +1,15 @@
+package com.tnsif.one;
+
+public class Constructors {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+		
+		int c = 5;
+		System.out.println(c);
+		
+		
+	}
+
+}
